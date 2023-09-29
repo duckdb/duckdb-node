32b15555033dcc2957106e7d26d871a15c7cda87
Hannes Mühleisen <hannes@muehleisen.org>
2023-09-29 09:21:12 +0200

    fix parquet test

--- test/parquet.test.ts
+++ test/parquet.test.ts
@@ -8,7 +8,7 @@ describe('can query parquet', function() {
     });
 
     it('should be able to read parquet files', function(done) {
-        db.run("select * from parquet_scan('../../data/parquet-testing/userdata1.parquet')", done);
+        db.run("select * from parquet_scan('test/userdata1.parquet')", done);
     });
 
 });
