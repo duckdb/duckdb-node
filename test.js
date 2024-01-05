5d6c09bf18c9d437e2a534f4ed1781affa557478
Hannes Mühleisen <hannes@duckdblabs.com>
2024-01-05 14:15:09 +0100

    more small refactoring on the way to more types

--- test.js
+++ test.js
@@ -1,7 +1,129 @@
 const duckdb_native = require('.');
+const {BigInt64Array} = require("apache-arrow/util/compat");
+const {Float32, Float64} = require("apache-arrow");
 
 // some warmup
 console.log("DuckDB version:", duckdb_native.duckdb_library_version());
+
+function convert_primitive_vector(vector, n, array_type) {
+    // seems ugly as hell but should be jit-ted away
+    var typed_validity_buf;
+    const validity_buf = duckdb_native.copy_buffer(duckdb_native.duckdb_vector_get_validity(vector),
+                                                   Math.ceil(n / 64) * 8); // this will be null if all rows are valid
+    if (validity_buf != null) {
+        typed_validity_buf = new BigUint64Array(validity_buf.buffer);
+    }
+
+    const data_buf =
+        duckdb_native.copy_buffer(duckdb_native.duckdb_vector_get_data(vector), array_type.BYTES_PER_ELEMENT * n);
+    const typed_data_arr = new array_type(data_buf.buffer);
+    const vector_data = new Array(n)
+    for (let row_idx = 0; row_idx < n; row_idx++) {
+        // interpret validity mask bytes
+        const row_is_valid =
+            validity_buf == null || (typed_validity_buf[Math.floor(row_idx / 64)] & (1n << BigInt(row_idx % 64))) > 0;
+        vector_data[row_idx] = row_is_valid ? typed_data_arr[row_idx] : null;
+    }
+    return vector_data;
+}
+
+function convert_vector(vector, n) {
+    const type = duckdb_native.duckdb_vector_get_column_type(vector);
+    const type_id = duckdb_native.duckdb_get_type_id(type);
+
+    switch (type_id) {
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_BIGINT:
+        return convert_primitive_vector(vector, n, BigInt64Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_BIT: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_BOOLEAN:
+        return convert_primitive_vector(vector, n, Uint8Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_DATE: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_DECIMAL: {
+        // UGH
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_DOUBLE:
+        return convert_primitive_vector(vector, n, Float64Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_ENUM: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_FLOAT:
+        return convert_primitive_vector(vector, n, Float32Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_HUGEINT: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_INTEGER:
+        return convert_primitive_vector(vector, n, Int32Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_INTERVAL: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_LIST: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_MAP: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_SMALLINT:
+        return convert_primitive_vector(vector, n, Int16Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_STRUCT: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TIME: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TIMESTAMP: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TIMESTAMP_MS: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TIMESTAMP_NS: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TIMESTAMP_S: {
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_TINYINT:
+        return convert_primitive_vector(vector, n, Int8Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_UBIGINT:
+        return convert_primitive_vector(vector, n, BigUint64Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_UINTEGER:
+        return convert_primitive_vector(vector, n, Uint32Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_UNION:
+        break;
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_USMALLINT:
+        return convert_primitive_vector(vector, n, Uint16Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_UTINYINT:
+        return convert_primitive_vector(vector, n, Uint8Array);
+
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_UUID: {
+        // array of arrays?
+        break;
+    }
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_BLOB:
+    case duckdb_native.duckdb_type.DUCKDB_TYPE_VARCHAR:
+        return duckdb_native.convert_string_vector(vector, n);
+
+    default:
+        console.log('Unsupported type :/');
+        return null;
+    }
+}
 
 async function test() {
 
@@ -37,7 +159,7 @@ async function test() {
     const prepared_statement = new duckdb_native.duckdb_prepared_statement;
     const prepare_status = await duckdb_native.duckdb_prepare(
         con,
-        "SELECT range, CASE WHEN range % 2 == 0 THEN range ELSE NULL END, CASE WHEN range % 2 == 0 THEN range::VARCHAR ELSE NULL END FROM range(?)",
+        "SELECT range::INTEGER, CASE WHEN range % 2 == 0 THEN range ELSE NULL END, CASE WHEN range % 2 == 0 THEN range::VARCHAR ELSE NULL END FROM range(?)",
         prepared_statement);
     if (prepare_status != duckdb_native.duckdb_state.DuckDBSuccess) {
         console.error(duckdb_native.duckdb_prepare_error(prepared_statement));
@@ -92,46 +214,7 @@ async function test() {
 
         // loop over columns and interpret vector bytes
         for (let col_idx = 0; col_idx < duckdb_native.duckdb_data_chunk_get_column_count(chunk); col_idx++) {
-            const vector = duckdb_native.duckdb_data_chunk_get_vector(chunk, col_idx);
-            const type = duckdb_native.duckdb_vector_get_column_type(vector);
-            const type_id = duckdb_native.duckdb_get_type_id(type);
-
-            switch (type_id) {
-            case duckdb_native.duckdb_type.DUCKDB_TYPE_BIGINT: {
-                // seems ugly as hell but should be jit-ted away
-                var typed_validity_buf;
-                const validity_buf =
-                    duckdb_native.copy_buffer(duckdb_native.duckdb_vector_get_validity(vector),
-                                              Math.ceil(n / 64) * 8); // this will be null if all rows are valid
-                if (validity_buf != null) {
-                    typed_validity_buf = new BigUint64Array(validity_buf.buffer);
-                }
-
-                const data_buf = duckdb_native.copy_buffer(duckdb_native.duckdb_vector_get_data(vector), 8 * n);
-                const typed_data_arr = new BigInt64Array(data_buf.buffer);
-                const vector_data = new Array(n)
-                for (let row_idx = 0; row_idx < n; row_idx++) {
-                    // interpret validity mask bytes
-                    const row_is_valid = validity_buf == null || (typed_validity_buf[Math.floor(row_idx / 64)] &
-                                                                  (1n << BigInt(row_idx % 64))) > 0;
-                    vector_data[row_idx] = row_is_valid ? typed_data_arr[row_idx] : null;
-                }
-
-                console.log(vector_data);
-                break;
-            }
-            case duckdb_native.duckdb_type.DUCKDB_TYPE_BLOB:
-            case duckdb_native.duckdb_type.DUCKDB_TYPE_VARCHAR: {
-                // strings are a special case because they (and only they) can contain pointers
-                // hence we have a special conversion function
-                console.log(duckdb_native.convert_string_vector(vector, n));
-                break;
-            }
-                // TODO strings, structs, lists, maps, unions, ...
-            default:
-                console.log('Unsupported type :/');
-                return;
-            }
+            console.log(convert_vector(duckdb_native.duckdb_data_chunk_get_vector(chunk, col_idx), n));
         }
 
         duckdb_native.duckdb_destroy_data_chunk(chunk);
