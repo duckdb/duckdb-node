4d12faa8de1a29c25b9cd00462afe6822304b5cf
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-02-19 13:45:00 +0100

    Change deployment from s3 bucket to npm.duckdb.org

--- scripts/node_build_win.sh
+++ scripts/node_build_win.sh
@@ -15,7 +15,18 @@ if [[ ! "$GITHUB_REF" =~ ^(refs/tags/v.+)$ ]] ; then
 fi
 npx node-pre-gyp package testpackage testbinary
 
+LOCAL_BINARY=$(./node_modules/.bin/node-pre-gyp reveal staged_tarball --silent)
+REMOTE_BINARY=$(./node_modules/.bin/node-pre-gyp reveal hosted_tarball --silent)
+s3_ENDPOINT_BINARY="s3://duckdb-npm/"${REMOTE_BINARY:23}
+
+pip install awscli
+
+echo "local binary at  $LOCAL_BINARY"
+echo "remote binary at $REMOTE_BINARY"
+echo "served from      $S3_ENDPOINT_BINARY"
+
 if [[ "$GITHUB_REF" =~ ^(refs/heads/main|refs/tags/v.+)$ ]] ; then
-  npx node-pre-gyp publish
-  npx node-pre-gyp info
+  aws s3 cp $LOCAL_BINARY $S3_ENDPOINT_BINARY --acl public-read
+else
+  aws s3 cp $LOCAL_BINARY $S3_ENDPOINT_BINARY --acl public-read --dryrun
 fi
