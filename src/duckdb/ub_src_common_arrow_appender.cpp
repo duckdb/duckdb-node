1f17e185a6014312b75b24e8e45e4903ae0979c0
Hannes Mühleisen <hannes@muehleisen.org>
2025-01-15 13:23:00 +0100

    zap duckdb

--- src/duckdb/ub_src_common_arrow_appender.cpp
+++
@@ -1,8 +0,0 @@
-#include "src/common/arrow/appender/bool_data.cpp"
-
-#include "src/common/arrow/appender/struct_data.cpp"
-
-#include "src/common/arrow/appender/union_data.cpp"
-
-#include "src/common/arrow/appender/fixed_size_list_data.cpp"
-
--- src/duckdb/ub_src_execution_operator_csv_scanner_table_function.cpp
+++
@@ -1,4 +0,0 @@
-#include "src/execution/operator/csv_scanner/table_function/csv_file_scanner.cpp"
-
-#include "src/execution/operator/csv_scanner/table_function/global_csv_state.cpp"
-
